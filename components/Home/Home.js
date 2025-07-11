10873306d28186b3df69df3bf4a7e0a024d959bd
danvazsan2 <danielvazquezsanchez13@gmail.com>
2025-07-11 14:46:56 +0200

    Simplify Home and Index. The template provided too much content at index page. I decided to make a more minimalist version of it.

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -7,31 +7,16 @@ import Sal from "sal.js";
 
 
 
-import SplitImg from "../../public/images/split/split-2.png";
-import SplitLogo from "../../public/images/split/split-2-logo.png";
-import DarkSplitImg from "../../public/images/light/split/split-2.png";
-import DarkSplitLogo from "../../public/images/light/split/split-2-logo.png";
 import bannerImg from "../../public/images/bg/slider-main-image.png";
 import bannerWhiteImg from "../../public/images/light/bg/slider-main-image.png";
 import shapeOne from "../../public/images/bg/icon-shape/icon-shape-one.png";
 import shapeTwo from "../../public/images/bg/icon-shape/icon-shape-two.png";
 import shapeThree from "../../public/images/bg/icon-shape/icon-shape-three.png";
 import shapeFour from "../../public/images/bg/icon-shape/icon-shape-four.png";
-import bgShape from "../../public/images/bg/split-bg-shape.png";
 import bgShapeOne from "../../public/images/bg/bg-shape-four.png";
 import bgShapeTwo from "../../public/images/bg/bg-shape-five.png";
-import bgShapeThree from "../../public/images/bg/bg-shape-two.png";
 
-import BrandList from "../Brands/BrandList";
 import TabStyleOne from "../TabStyles/TabStyle-One";
-import ServiceStyleOne from "../Services/ServiceStyle-One";
-import AdvanceTab from "../TabStyles/AdvanceTab";
-import CtaOne from "../CallToActions/Cta-One";
-
-import ServiceTwo from "../Services/Service-Two";
-import Testimonial from "../Testimonials/Testimonial";
-import BrandTwo from "../Brands/Brand-Two";
-import CtaTwo from "../CallToActions/Cta-Two";
 import { useAppContext } from "@/context/Context";
 
 const Home = () => {
@@ -52,6 +37,7 @@ const Home = () => {
 
   return (
     <>
+      {/* Hero/Banner Section */}
       <div
         className="slider-area slider-style-1 variation-default slider-bg-image bg-banner1 slider-bg-shape"
         data-black-overlay="1"
@@ -178,30 +164,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="rainbow-brand-area rainbow-section-gap">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div
-                className="section-title rating-title text-center sal-animate"
-                data-sal="slide-up"
-                data-sal-duration="700"
-                data-sal-delay="100"
-              >
-                <p className="b1 mb--0 small-title">
-                  truest 800,000+ HIGHLY PRODUCTIVE Company
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-12 mt--10">
-              <BrandList />
-            </div>
-          </div>
-        </div>
-      </div>
-
+      {/* Service Section with Tabs */}
       <div className="rainbow-service-area rainbow-section-gap">
         <div className="container">
           <div className="row">
@@ -224,176 +187,6 @@ const Home = () => {
             </div>
           </div>
           <TabStyleOne />
-        </div>
-      </div>
-
-      <div className="rainbow-service-area rainbow-section-gap rainbow-section-gapBottom-big">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div
-                className="section-title text-left"
-                data-sal="slide-up"
-                data-sal-duration="400"
-                data-sal-delay="150"
-              >
-                <h4 className="subtitle">
-                  <span className="theme-gradient">Assisting individuals</span>
-                </h4>
-                <h2 className="title mb--60">
-                  Chat Smarter, Not <br /> Harder with
-                </h2>
-              </div>
-            </div>
-          </div>
-        </div>
-        <ServiceStyleOne />
-      </div>
-
-      <div className="rainbow-advance-tab-area aiwave-bg-gradient rainbow-section-gap-big">
-        <div className="container">
-          <div className="html-tabs" data-tabs="true">
-            <AdvanceTab />
-          </div>
-        </div>
-        <div className="bg-shape">
-          <Image src={bgShape} width={630} height={879} alt="Bg Shape" />
-        </div>
-      </div>
-
-      <div className="rainbow-collobration-area rainbow-section-gap-big">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div
-                className="section-title text-center"
-                data-sal="slide-up"
-                data-sal-duration="700"
-                data-sal-delay="100"
-              >
-                <h4 className="subtitle ">
-                  <span className="theme-gradient">AI Collaboration</span>
-                </h4>
-                <h2 className="title mb--20">
-                  AI Chat app for seamless
-                  <br /> collaboration
-                </h2>
-                <Link
-                  className="btn-default btn-large color-blacked"
-                  href="/contact"
-                >
-                  Try It Now{" "}
-                  <i className="fa-sharp fa-light fa-arrow-right ml--5"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-12 mt--60">
-              <div className="collabration-image-section">
-                <Image
-                  src={isLightTheme ? SplitImg : DarkSplitImg}
-                  width={1305}
-                  height={712}
-                  alt="collabration-image"
-                />
-                <div className="logo-section">
-                  <div className="center-logo">
-                    <Image
-                      src={isLightTheme ? SplitLogo : DarkSplitLogo}
-                      width={104}
-                      height={143}
-                      alt="Small Logo"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="rainbow-rn-cta">
-        <div className="container">
-          <CtaOne />
-        </div>
-      </div>
-
-      <div className="aiwave-service-area rainbow-section-gap">
-        <div className="container">
-          <div className="row row--15 service-wrapper">
-            <ServiceTwo />
-          </div>
-        </div>
-      </div>
-
-      <div className="rainbow-testimonial-area rainbow-section-gap">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div
-                className="section-title text-left"
-                data-sal="slide-up"
-                data-sal-duration="400"
-                data-sal-delay="150"
-              >
-                <h4 className="subtitle">
-                  <span className="theme-gradient">Assisting individuals</span>
-                </h4>
-                <h2 className="title mb--60">The opinions of the community</h2>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Testimonial />
-      </div>
-
-      <div className="rainbow-brand-area rainbow-section-gap">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div
-                className="section-title rating-title text-center sal-animate"
-                data-sal="slide-up"
-                data-sal-duration="700"
-                data-sal-delay="100"
-              >
-                <div className="rating">
-                  <a href="#rating">
-                    <i className="fa-sharp fa-solid fa-star"></i>
-                  </a>
-                  <a href="#rating">
-                    <i className="fa-sharp fa-solid fa-star"></i>
-                  </a>
-                  <a href="#rating">
-                    <i className="fa-sharp fa-solid fa-star"></i>
-                  </a>
-                  <a href="#rating">
-                    <i className="fa-sharp fa-solid fa-star"></i>
-                  </a>
-                  <a href="#rating">
-                    <i className="fa-sharp fa-solid fa-star"></i>
-                  </a>
-                </div>
-                <p className="subtitle mb--0">Based on 20,000+ reviews on</p>
-              </div>
-            </div>
-          </div>
-          <BrandTwo />
-          <div className="bg-shape-left">
-            <Image
-              src={bgShapeThree}
-              width={688}
-              height={1055}
-              alt="Bg shape"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="rainbow-cta-area rainbow-section-gap rainbow-section-gapBottom-big">
-        <div className="container">
-          <CtaTwo />
         </div>
       </div>
     </>
